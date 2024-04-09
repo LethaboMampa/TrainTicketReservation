6f79dd4855dc2ee83f750c9ff9447e9e568e8ea6
LethaboMampa <119916702+LethaboMampa@users.noreply.github.com>
2024-04-09 10:04:46 +0200

    Backend Update

---
+++ src/main/java/com/example/TrainResevation/TrainResevationApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrainResevation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainResevationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainResevationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrainResevation/TrainResevationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrainResevation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainResevationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
